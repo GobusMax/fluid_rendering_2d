5c25880ad62dd476f4811ef6e412956ef9fe0f6d
Florian Hoffmann <florih@pm.me>
2023-08-07 17:12:50 +0200

    input stuff

--- src/bin/smooth_max_min.rs
+++ src/bin/smooth_max_min.rs
@@ -4,6 +4,13 @@ use macroquad::prelude::*;
 const RADIUS: f32 = 4.;
 const MIN_DISTANCE: f64 = 32.;
 
+#[derive(Debug)]
+enum MinFunction {
+    Boltzman,
+    LogSumExp,
+    Mellowmax,
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut points = vec![DVec2::NAN; 10];
@@ -16,18 +23,59 @@ async fn main() {
             vec2(screen_width() * 3. / 4., screen_height() * 3. / 4.).as_dvec2(),
         );
     }
+    let mut alpha: f64 = -0.1;
+    let mut min_distance: f64 = 32.;
+    let mut smooth_function = MinFunction::Boltzman;
     loop {
         clear_background(WHITE);
         image = Image::gen_image_color(screen_width() as _, screen_height() as _, WHITE);
         points[0] = Vec2::from(mouse_position()).as_dvec2();
+        if is_key_down(KeyCode::Down) {
+            alpha -= 0.01;
+            alpha = alpha.clamp(-4., 0.);
+        }
+        if is_key_down(KeyCode::Up) {
+            alpha += 0.01;
+            alpha = alpha.clamp(-4., 0.);
+        }
+        if is_key_down(KeyCode::Left) {
+            min_distance -= 1.;
+        }
+        if is_key_down(KeyCode::Right) {
+            min_distance += 1.;
+        }
+        if is_key_pressed(KeyCode::Key1) {
+            smooth_function = MinFunction::Boltzman
+        }
+        if is_key_pressed(KeyCode::Key2) {
+            smooth_function = MinFunction::LogSumExp
+        }
+        if is_key_pressed(KeyCode::Key3) {
+            smooth_function = MinFunction::Mellowmax
+        }
+        if is_key_pressed(KeyCode::R) {
+            for v in &mut points {
+                *v = random_vec(
+                    vec2(screen_width() / 4., screen_width() / 4.).as_dvec2(),
+                    vec2(screen_width() * 3. / 4., screen_height() * 3. / 4.).as_dvec2(),
+                );
+            }
+        }
+
         for x in 0..screen_width() as usize {
             for y in 0..screen_height() as usize {
                 let diff: Vec<_> = points
                     .iter()
                     .map(|v| v.distance(dvec2(x as _, y as _)))
                     .collect();
-                let res = mellowmax(&diff, -0.1);
-                if res <= MIN_DISTANCE {
+
+                let res = match smooth_function {
+                    MinFunction::Boltzman => boltzman(&diff, alpha),
+                    MinFunction::LogSumExp => log_sum_exp(&diff, alpha),
+                    MinFunction::Mellowmax => mellowmax(&diff, alpha),
+                };
+
+                if res <= min_distance {
                     image.set_pixel(x as _, y as _, GREEN);
                 }
             }
@@ -37,6 +85,21 @@ async fn main() {
         for v in &points {
             draw_circle(v.x as _, v.y as _, RADIUS, RED);
         }
+        draw_text(format!("Alpha: {}", alpha).as_str(), 10., 10., 16., BLACK);
+        draw_text(
+            format!("Min Distance: {}", min_distance).as_str(),
+            10.,
+            26.,
+            16.,
+            BLACK,
+        );
+        draw_text(
+            format!("Smooth Function: {:?}", smooth_function).as_str(),
+            10.,
+            42.,
+            16.,
+            BLACK,
+        );
         next_frame().await
     }
 }
@@ -44,8 +107,8 @@ fn window_conf() -> Conf {
     Conf {
         window_title: "Window name".to_owned(),
         fullscreen: false,
-        window_width: 200,
-        window_height: 200,
+        window_width: 256,
+        window_height: 256,
         ..Default::default()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,41 @@
-use fluid_rendering_2d::random_vec;
+use fluid_rendering_2d::{mellowmax, random_vec};
 use macroquad::prelude::*;
 
-const FOV: f32 = 90.;
-const NUM_RAYS: usize = 1;
-const STOPPING_DISTANCE: f32 = 10.;
+const FOV: f64 = 90.;
+const NUM_RAYS: usize = 100;
+const STOPPING_DISTANCE: f64 = 10.;
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let mut points = vec![Vec2::NAN; 100];
-    let mut pos = Vec2::ZERO;
+    let mut points = vec![DVec2::NAN; 100];
+    let mut pos = DVec2::ZERO;
 
     for v in &mut points {
         *v = random_vec(
             vec2(screen_width() / 4., screen_width() / 4.).as_dvec2(),
             vec2(screen_width() * 3. / 4., screen_height() * 3. / 4.).as_dvec2(),
-        )
-        .as_vec2();
+        );
     }
     loop {
         clear_background(WHITE);
         if is_mouse_button_pressed(MouseButton::Left) {
-            pos = mouse_position().into();
+            pos = Vec2::from(mouse_position()).as_dvec2();
         }
         for v in &points {
-            draw_circle(v.x, v.y, STOPPING_DISTANCE, RED);
+            draw_circle(v.x as f32, v.y as f32, STOPPING_DISTANCE as f32, RED);
         }
-        let dir = (Vec2::from(mouse_position()) - pos).normalize();
+        let dir = (Vec2::from(mouse_position()).as_dvec2() - pos).normalize();
 
         let res = multi_cast(pos, dir, &points, STOPPING_DISTANCE);
         for (i, v) in res.iter().enumerate() {
             let color = if !v.is_nan() {
-                draw_circle(v.x, v.y, 5., GREEN);
+                draw_circle(v.x as f32, v.y as f32, 5., GREEN);
                 let dist_scaled = 1. - (v.distance(pos) / 100.).tanh();
-                Color::new(dist_scaled, dist_scaled, dist_scaled, 1.)
+                Color::new(
+                    dist_scaled as f32,
+                    dist_scaled as f32,
+                    dist_scaled as f32,
+                    1.,
+                )
             } else {
                 RED
             };
@@ -48,32 +52,34 @@ async fn main() {
 }
 
 fn sphere_tracing(
-    start: Vec2,
-    dir: Vec2,
-    points: &Vec<Vec2>,
-    stopping_distance: f32,
+    start: DVec2,
+    dir: DVec2,
+    points: &[DVec2],
+    stopping_distance: f64,
     draw_circles: bool,
-) -> Vec2 {
+) -> DVec2 {
     let mut pos = start;
     let dir_normalized = dir.normalize_or_zero();
     draw_line(
-        start.x,
-        start.y,
-        start.x + dir_normalized.x * 1000.,
-        start.y + dir_normalized.y * 1000.,
+        start.x as f32,
+        start.y as f32,
+        (start.x + dir_normalized.x * 1000.) as f32,
+        (start.y + dir_normalized.y * 1000.) as f32,
         1.,
         BLACK,
     );
     for _ in 0..100 {
-        let mut min_dist = f32::INFINITY;
+        let mut min_dist = f64::INFINITY;
         for p in points {
             let dist = p.distance(pos);
             if dist < min_dist {
                 min_dist = dist;
             }
         }
+        // let diff: Vec<_> = points.iter().map(|v| v.distance(pos)).collect();
+        // min_dist = mellowmax(&diff, -0.1);
         if draw_circles {
-            draw_circle_lines(pos.x, pos.y, min_dist, 1., GREEN);
+            draw_circle_lines(pos.x as f32, pos.y as f32, min_dist as f32, 1., GREEN);
         }
         if min_dist <= stopping_distance * 1.001 {
             return pos;
@@ -82,70 +88,31 @@ fn sphere_tracing(
         }
     }
     draw_line(
-        start.x,
-        start.y,
-        start.x + dir_normalized.x * 1000.,
-        start.y + dir_normalized.y * 1000.,
+        (start.x) as f32,
+        (start.y) as f32,
+        (start.x + dir_normalized.x * 1000.) as f32,
+        (start.y + dir_normalized.y * 1000.) as f32,
         1.,
         RED,
     );
-    Vec2::NAN
+    DVec2::NAN
 }
 
-fn test_tracing(
-    start: Vec2,
-    dir: Vec2,
-    points: &[Vec2],
-    stopping_distance: f32,
-    draw_circles: bool,
-) -> Vec2 {
-    let mut pos = start;
-    let dir_normalized = dir.normalize_or_zero();
-    draw_line(
-        start.x,
-        start.y,
-        start.x + dir_normalized.x * 1000.,
-        start.y + dir_normalized.y * 1000.,
-        1.,
-        BLACK,
-    );
-    for _ in 0..100 {
-        let res_dist = points.iter().fold(0., |a, v| a + (*v - pos).length());
-        if draw_circles {
-            draw_circle_lines(pos.x, pos.y, res_dist, 1., GREEN);
-        }
-        if res_dist <= stopping_distance * 1.001 {
-            return pos;
-        } else {
-            pos += dir_normalized * (res_dist - stopping_distance);
-        }
-    }
-    draw_line(
-        start.x,
-        start.y,
-        start.x + dir_normalized.x * 1000.,
-        start.y + dir_normalized.y * 1000.,
-        1.,
-        RED,
-    );
-    Vec2::NAN
-}
-
-fn multi_cast(start: Vec2, dir: Vec2, points: &[Vec2], stopping_distance: f32) -> Vec<Vec2> {
+fn multi_cast(start: DVec2, dir: DVec2, points: &[DVec2], stopping_distance: f64) -> Vec<DVec2> {
     let mut res = vec![];
 
     let forward = dir.normalize();
     let right = -forward.perp();
-    let tranform = mat2(forward, right);
+    let tranform = dmat2(forward, right);
     let dist = 0.5 / (FOV * 0.5).to_radians().tan();
 
     for i in 0..NUM_RAYS {
         let ray_dir = if NUM_RAYS > 1 {
-            tranform.mul_vec2(vec2(dist, (i as f32 / (NUM_RAYS - 1) as f32) - 0.5).normalize())
+            tranform.mul_vec2(dvec2(dist, (i as f64 / (NUM_RAYS - 1) as f64) - 0.5).normalize())
         } else {
             forward
         };
-        let intersection = test_tracing(start, ray_dir, points, stopping_distance, true);
+        let intersection = sphere_tracing(start, ray_dir, points, stopping_distance, true);
         res.push(intersection);
     }
 
